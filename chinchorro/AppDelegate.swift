82af7f7b1857714ac791f7bab504d4a395d465d3
Marco Carmona <elbuki@users.noreply.github.com>
2024-08-28 14:46:07 -0600

    add environment initializing

--- chinchorro/AppDelegate.swift
+++ chinchorro/AppDelegate.swift
@@ -9,24 +9,22 @@ import GoogleMaps
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     
-    let googleSDKTokenKey = "GOOGLE_MAPS_SDK_TOKEN"
-    
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
     ) -> Bool {
         
-        guard let token = ProcessInfo.processInfo.environment[googleSDKTokenKey] else {
-            fatalError("google maps token is not defined")
+        do {
+            let environmentValues = try Environment()
+            
+            guard GMSServices.provideAPIKey(environmentValues.googleMapsSDKToken) else {
+                fatalError("could not provide the api key to the google maps service")
+            }
+            
+            return true
+        } catch {
+            fatalError("could not initialize environment values: \(error.localizedDescription)")
         }
-        
-        
-
-        guard GMSServices.provideAPIKey(token) else {
-            fatalError("could not provide the api key to the google maps service")
-        }
-        
-        return true
         
     }
     
--- chinchorro/Environment.swift
+++ chinchorro/Environment.swift
@@ -9,4 +9,20 @@ import Foundation
 
 struct Environment: Decodable {
     let googleMapsSDKToken: String
+    
+    enum InitError: Error {
+        case envFileNotFound
+    }
+    
+    init() throws {
+        guard let filePath = Bundle.main.path(forResource: "env", ofType: "json") else {
+            throw InitError.envFileNotFound
+        }
+        
+        let fileURL = URL(fileURLWithPath: filePath)
+        let data = try Data(contentsOf: fileURL)
+        let decoded = try JSONDecoder().decode(Environment.self, from: data)
+        
+        self = decoded
+    }
 }
