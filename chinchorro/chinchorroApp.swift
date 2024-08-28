4ea80761ed9217c7ad3f24bbe1b9f5ad1679f6e3
Marco Carmona <elbuki@users.noreply.github.com>
2024-08-28 14:04:37 -0600

    Initial Commit

---
+++ chinchorro/chinchorroApp.swift
@@ -0,0 +1,17 @@
+//
+//  chinchorroApp.swift
+//  chinchorro
+//
+//  Created by Marco Carmona on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct chinchorroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
